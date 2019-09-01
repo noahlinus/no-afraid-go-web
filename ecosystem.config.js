c9c9787e8246ed2479f993b87d1d25110974e6c1
林大元 <519872612@qq.com>
2019-09-01 14:32:00 +0800

    修改问题

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -20,14 +20,14 @@ module.exports = {
     },
   ],
 
-  // deploy : {
-  //   production : {
-  //     user : 'node',
-  //     host : '212.83.163.1',
-  //     ref  : 'origin/master',
-  //     repo : 'git@github.com:repo.git',
-  //     path : '/var/www/production',
-  //     'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
-  //   }
-  // }
+  deploy : {
+    production : {
+      user : 'root',
+      host : '111.230.28.25',
+      ref  : 'origin/master',
+      repo : 'git@github.com:repo.git',
+      path : '/var/www/production',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    }
+  }
 }
